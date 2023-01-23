b26c4ce288f57baacf5780e0c8e779a5935097ab
Victoria Sandford <115006162+VictoriaKinkajou@users.noreply.github.com>
2023-01-23 11:56:34 +0000

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -13,7 +13,7 @@ class Scraper():
         self.url_to_scrape = url
         self.dict_products = {'Image': [], 'Product Name': [], 'Price': [], 'Description': [], 'Rating': [], 'Timestamp': [], 'ID': []}
 
-    def accept_cookies(self):
+    def __accept_cookies(self):
         '''
         This method accepts the cookies by clicking on the button if displayed.
         '''
@@ -28,18 +28,18 @@ class Scraper():
         except:
             pass 
     
-    def open_results_page(self):
+    def __open_results_page(self):
         '''
         This method opens a page of products; maximises the browser to fit more products on the page; and calls scroll_down_all to reveal all the products.
         '''
         self.driver.maximize_window()
         self.driver.get(self.url_to_scrape)
         time.sleep(2)
-        self.accept_cookies()
+        self.__accept_cookies()
         time.sleep(1)
-        self.scroll_down_all()
+        self.__scroll_down_all()
     
-    def scroll_down_all(self): 
+    def __scroll_down_all(self): 
         '''
         If there more products than fit in the browser window, you need to keeping scrolling down to the bottom of the page to reveal them all. 
         This method performs that task, by alternately scolling and pausing to give the system time to load the results.
@@ -50,12 +50,12 @@ class Scraper():
             time.sleep(2)
             self.driver.execute_script("window.scrollTo(0, {});".format(i))
 
-    def create_product_url_list(self):
+    def __create_product_url_list(self):
         '''
         This method finds the URL of the individual product page for each search result.
         This also adds three recommended products that we don't want to scrape data for, so these are removed from the list.
         '''
-        self.open_results_page()
+        self.__open_results_page()
         product_container = self.driver.find_elements(by=By.XPATH, value='//div[@class="fop-contentWrapper"]')
         url_list_including_recommended_products = []
 
@@ -64,11 +64,10 @@ class Scraper():
             link = a_tag.get_attribute("href")
             url_list_including_recommended_products.append(link)
         self.product_url_list = url_list_including_recommended_products[3:]
-        print(self.product_url_list)
-        print(f'There are {len(self.product_url_list)} products on this page.')
+
         return self.product_url_list
 
-    def get_image_url(self):
+    def __get_image_url(self):
         '''
         This method finds the source URL of the product image and adds it to the dictionary.
         ''' 
@@ -77,7 +76,7 @@ class Scraper():
         self.image_src = image_container.get_attribute("src")
         self.dict_products['Image'].append(self.image_src)
 
-    def get_text(self):
+    def __get_text(self):
         '''
         This method finds the name of the product (including volume), the price, the description text and the customer rating (if it has one), and adds them to the dictionary.
         '''
@@ -99,7 +98,7 @@ class Scraper():
             self.no_rating = 'NA'
             self.dict_products['Rating'].append(self.no_rating)
 
-    def get_timestamp(self):
+    def __get_timestamp(self):
         '''
         This method adds a timestamp of when the data was scraped to the dictionary.
         '''
@@ -107,7 +106,7 @@ class Scraper():
         self.timestamp = datetime.datetime.now()
         self.dict_products['Timestamp'].append(self.timestamp.strftime('%Y-%m-%d %H:%M:%S'))
 
-    def get_product_id(self):
+    def __get_product_id(self):
         '''
         This method finds the product ID number, by taking it from the end of the product page URL, and adds it to the dictionary.
         '''
@@ -119,7 +118,7 @@ class Scraper():
             self.product_id = product_id_with_hyphen
         self.dict_products['ID'].append(self.product_id)
 
-    def create_raw_data_folder(self):
+    def __create_raw_data_folder(self):
         '''
         This method creates a folder named "raw_data" if it doesn't already exist.
         '''
@@ -130,7 +129,7 @@ class Scraper():
         else:
             print('Raw data folder already exists!')
     
-    def create_id_subfolder(self):
+    def __create_id_subfolder(self):
         '''
         This method creates a subfolder in the "raw_data" folder, with the product ID number as its name.
         '''
@@ -141,7 +140,7 @@ class Scraper():
         else:
             print('ID folder already exists!')
 
-    def save_image(self): 
+    def __save_image(self): 
         '''
         This method creates a subfolder named "images" in the product ID folder. Then it downloads the product image jpeg from the source URL and saves it in the "images" folder.
         '''
@@ -151,22 +150,23 @@ class Scraper():
             print('Creating images folder')
         
         image_url_list = self.dict_products['Image']
-        image_url = image_url_list[0]
-        img_data = requests.get(image_url).content
+        for image_url in image_url_list:
         
-        with open(f'raw_data\\{self.product_id}\\images\\Product Image.jpg', 'wb') as handler:
-            handler.write(img_data)
-        print('Product image saved in images folder')
+            img_data = requests.get(image_url).content
+        
+            with open(f'raw_data\\{self.product_id}\\images\\Product Image.jpg', 'wb') as handler:
+                handler.write(img_data)
+            print(f'Product image saved in {self.product_id} folder')
 
-    def save_dictionary_to_json_file(self):
+    def __save_dictionary(self):
         '''
-        This method saves the dictionary in a json file and stores it in the product id subfolder.
+        This method creates folders, saves the dictionary in a json file and stores it in the product id subfolder.
         '''
-        self.create_raw_data_folder()
-        self.create_id_subfolder()
+        self.__create_raw_data_folder()
+        self.__create_id_subfolder()
         with open(f'raw_data\\{self.product_id}\\json_data.json', 'w') as f:
             json.dump(self.dict_products, f, indent=6)
-        self.save_image()
+        self.__save_image()
         print('Saving dictionary in json file')
 
     def open_product_page(self):
@@ -174,21 +174,21 @@ class Scraper():
         This method begins the web scraping process. It calls the method that creates a list of product page urls. 
         It then opens each page and scrapes the data.
         '''
-        self.create_product_url_list()
+        self.__create_product_url_list()
 
         for self.product_url in self.product_url_list:
             self.driver.get(self.product_url)
-            self.scrape_data()
+            self.__scrape_data()
         
-    def scrape_data(self):
+    def __scrape_data(self):
         '''
         This function calls all the methods to collect and store the data and image.
         '''
-        self.get_image_url()
-        self.get_text()
-        self.get_timestamp()
-        self.get_product_id()
-        self.save_dictionary_to_json_file()
+        self.__get_image_url()
+        self.__get_text()
+        self.__get_timestamp()
+        self.__get_product_id()
+        self.__save_dictionary()
     
     
 def scrape_all():
